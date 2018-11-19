9fd443bc6a7664edcdf605997c2f7af18293c2a0
miguel <mig.ruiz@gmail.com>
2018-11-19 13:09:21 +0000

    off fucntion

--- OffFx/index.js
+++ OffFx/index.js
@@ -1,6 +1,17 @@
-
-var mqtt = require('mqtt')
-var client  = mqtt.connect(global.mtqqURL) 
-client.on('connect', function () {
-    client.publish(global.turnOffLightsTopic, 'LightsOff')
-})
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.lightsoff = (req, res) => {
+    var mqtt = require('mqtt');
+    var client  = mqtt.connect(process.env.mtqqURL);
+  	console.log(process.env.mtqqURL);
+  	console.log(process.env.turnOffLightsTopic);
+    client.on('connect', function () {
+      console.log('entered');
+        client.publish(process.env.turnOffLightsTopic, 'LightsOff');
+        res.status(200).send('');
+    })
+  };
